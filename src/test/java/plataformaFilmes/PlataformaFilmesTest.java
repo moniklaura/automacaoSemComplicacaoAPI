34c08570a1373dab5387cb0b2b89cbbafda98f68
MonicaLaura <monik.laura2010@gmail.com>
2022-10-04 10:57:24 -0300

    aula 11 frameork e lista de itens

--- src/test/java/plataformaFilmes/PlataformaFilmesTest.java
+++ src/test/java/plataformaFilmes/PlataformaFilmesTest.java
@@ -5,8 +5,10 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import utils.RestUtils;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,14 +18,14 @@ public class PlataformaFilmesTest {
 
     @Test
     public void validarLogin() {
-        RestAssured.baseURI = "http://localhost:8080/";
+        RestUtils.setBaseURI("http://localhost:8080/");
 
         String json = "{" +
                 "\"email\": \"aluno@email.com\"," +
                 "\"senha\": \"123456\"" +
                 "}";
 
-        Response response = post(json, ContentType.JSON, "auth");
+        Response response = RestUtils.post(json, ContentType.JSON, "auth");
 
         assertEquals(200, response.statusCode());
         String token = response.jsonPath().get("token");
@@ -34,12 +36,12 @@ public class PlataformaFilmesTest {
     }
     @BeforeAll
     public static void validarLoginMap(){
-        RestAssured.baseURI = "http://localhost:8080/";
+        RestUtils.setBaseURI("http://localhost:8080/");
         Map<String, String> map = new HashMap<>();
         map.put("email", "aluno@email.com");
         map.put("senha", "123456");
 
-        Response response = post(map, ContentType.JSON, "auth");
+        Response response = RestUtils.post(map, ContentType.JSON, "auth");
 
         assertEquals(200, response.statusCode());
         token = response.jsonPath().get("token");
@@ -51,34 +53,17 @@ public class PlataformaFilmesTest {
         Map<String, String> header = new HashMap<>();
         header.put("Authorization", "Bearer " + token);
 
-        Response response = get(header,"categorias");
+        Response response = RestUtils.get(header,"categorias");
         assertEquals(200, response.statusCode());
         System.out.println(response.jsonPath().get().toString());
 
+        assertEquals("Terror", response.jsonPath().get("tipo[2]"));
+
+        List<String> listTipo = response.jsonPath().get("tipo");
+        assertTrue(listTipo.contains("Terror"), "não foi encontrado a categoria Romance");
+
+
 
     }
-
-    private static Response get(Map<String, String> header, String endpoint) {
-        return RestAssured.given()
-                .relaxedHTTPSValidation()
-                .headers(header)
-                .when()
-                .get(endpoint)
-                .thenReturn();
-    }
-
-    public static Response post(Object json, ContentType contentType, String endpoint) {
-        return RestAssured.given()
-                .relaxedHTTPSValidation()
-                .contentType(contentType)
-                .body(json)
-                .when()
-                .post(endpoint)
-                .thenReturn();
-
-    }
-
-
-
 
 }
